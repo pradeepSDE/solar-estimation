e30b9426e007fcb5a432e99f751d84b8deae3079
pradeep bisen <103747065+pradeepSDE@users.noreply.github.com>
2024-07-25 19:08:10 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const multer = require('multer');
 const app = express();
 const port = 3000;
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'solar-estimation-frontend.vercel.app',
     credentials: true,
     methods: ['POST', 'GET'],
     allowedHeaders: ["Content-Type", "Authorization"],
