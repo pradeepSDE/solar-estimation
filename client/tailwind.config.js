7d75db81ffc9dc7fe3b7bf7274b5919fadc4ecc5
pradeep <pradeepbisen60@gmail.com>
2024-07-25 08:02:51 +0530

    client side

---
+++ client/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'solar': "url(./src/assets/img2.jpg)" ,
+      },
+    },
+  },
+  plugins: [],
+}
+
