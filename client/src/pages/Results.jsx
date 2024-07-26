fde225530d21c7e24178f078c8313d6965c32762
pradeep <pradeepbisen60@gmail.com>
2024-07-26 13:52:50 +0530

    image path correction

--- client/src/pages/Results.jsx
+++ client/src/pages/Results.jsx
@@ -2,25 +2,16 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Results = () => {
-  const [imageUrl, setImageUrl] = useState('');
+  // const [imageUrl, setImageUrl] = useState('');
+  const imageUrl = 'https://solar-estimation-server.vercel.app/images/result.png';
 
-  useEffect(() => {
-    // Fetch the result image URL from your server endpoint
-      // setImageUrl(`https://solar-estimation-server.vercel.app/images/result.png`);
-     
-
-        axios.get('/images/result.png').then((response) => { 
-          
-      
-
-          
-          
-          setImageUrl(response.data);
-          console.log(response);
-        });
-      
+  // useEffect(() => {
+  //       // axios.get('/images/result.png').then((response) => { 
+  //       //   setImageUrl(response.data);
+  //       //   console.log(response);
+  //       // });
    
-  }, []);
+  // }, []);
 
   return (
     <div>
